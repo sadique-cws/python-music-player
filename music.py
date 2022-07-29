638c6da8a81491d1c45fcacf38ef8acc283854b4
sadique-cws <sadique.hussain96@gmail.com>
2022-07-29 17:40:43 +0530

    first commit

---
+++ music.py
@@ -0,0 +1,11 @@
+import imp
+from tkinter import Tk
+import tkinter
+
+root = Tk()
+root.title("cws player")
+root.geometry("920x670+290+85")
+root.config(bg="#0f1a2b")
+root.resizable(False,False)
+
+root.mainloop()
